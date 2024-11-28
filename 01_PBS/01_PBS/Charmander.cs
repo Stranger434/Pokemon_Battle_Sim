b51822d7fd28145cea9ab0c6e86014652b08654a
Tim Bos <99076495@mydavinci.nl>
2024-11-28 15:21:15 +0100

    Assignment 1 Classes and Objects

---
+++ 01_PBS/01_PBS/Charmander.cs
@@ -0,0 +1,32 @@
+﻿using System.Numerics;
+using System.Xml.Linq;
+
+class Charmander
+{
+    // Fields
+    public string nickname;
+    public string strenght;
+    public string weakness;
+
+    // Constructor
+    public Charmander(string nickname, string strenght, string weakness)
+    {
+        this.nickname = nickname;
+        this.strenght = strenght;
+        this.weakness = weakness;
+    }
+
+    //Methods
+    public void BattleCry()
+    {
+        Console.WriteLine(this.nickname);
+
+    }
+}
+
+//1.The player start the game.
+//2. The player gives a name to a charmander.
+//3. The charmander does its battle cry for ten times.
+//4. The player can give a new name to the same charmander.
+//5. The charmander does its battle cry for ten times.
+//6. Repeat 4 and 5 until the player quits the game.
---
+++ 01_PBS/01_PBS/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        while (true)
+        {
+            Console.WriteLine("Enter Charmanders Nickname");
+            string nickname = Console.ReadLine();
+
+            if (nickname == "quit" || nickname == "") { 
+                System.Environment.Exit(0);
+            }
+
+            Charmander charmander = new Charmander(nickname, "Fire", "Water");
+            Console.WriteLine(charmander.nickname + " " + charmander.strenght + " " + charmander.weakness);
+            for (int i = 0; i < 10; i++)
+            {
+                charmander.BattleCry();
+            }
+        }
+    }
+}   
