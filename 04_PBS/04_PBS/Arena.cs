5843db2ce1ab56c1c4c48dd089a8f60d2653ef48
Tim Bos <99076495@mydavinci.nl>
2025-01-21 13:11:16 +0100

    Assignment 4: Static variables and methods afgerond

---
+++ 04_PBS/04_PBS/Arena.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+public class Arena
+{
+    public static int TotalBattles;
+    public static int TotalRounds;
+    public static int trainer2points;
+    public static int trainer1points;
+
+
+    public static void StartBattleLoop(Trainer trainer1, Trainer trainer2, Pokemon pokemon1, Pokemon pokemon2, Pokemon pokemon3)
+    {
+        Random rnd = new Random();
+        TotalBattles += 1;
+
+        while (trainer1.Pokeballs.Count > 0 && trainer2.Pokeballs.Count > 0)
+        {
+            // Kies willekeurige Pokéball voor beide trainers
+            int index1 = rnd.Next(0, trainer1.Pokeballs.Count);
+            int index2 = rnd.Next(0, trainer2.Pokeballs.Count);
+
+            Pokemon p1 = (Pokemon)trainer1.Pokeballs[index1].Pokemon;
+            Pokemon p2 = (Pokemon)trainer2.Pokeballs[index2].Pokemon;
+
+            trainer1.ThrowPokeball(index1);
+            Console.WriteLine(PokemonCheck.Check(trainer1, p1, pokemon1, pokemon2, pokemon3));
+
+            trainer2.ThrowPokeball(index2);
+            Console.WriteLine(PokemonCheck.Check(trainer2, p2, pokemon1, pokemon2, pokemon3));
+
+            startBattle(p1, p2, trainer1, trainer2);
+        }
+
+        Console.WriteLine("Alle Pokéballs zijn opgebruikt. Het gevecht is voorbij!");
+        Console.WriteLine($"Totale gevechten: {TotalBattles}");
+        Console.WriteLine($"Totale rondes: {TotalRounds}");
+    }
+
+    public static void startBattle(Pokemon p1, Pokemon p2, Trainer trainer1, Trainer trainer2)
+    {
+        Battle battle = new Battle();
+        string result = battle.fight(p1, p2, trainer1, trainer2);
+        Console.WriteLine(result);
+        TotalRounds += battle.GetRounds();
+        Console.WriteLine(TotalRounds + " Rounds");
+    }
+}
---
+++ 04_PBS/04_PBS/Pokemons/Bulbasaur.cs
@@ -0,0 +1,13 @@
+﻿class Bulbasaur : Pokemon
+{
+    // Constructor
+    public Bulbasaur(string name, string strenght, string weakness) : base(name, strenght, weakness)
+    {
+    }
+
+    // Methods
+    public override string BattleCry()
+    {
+        return (this.Name);
+    }
+}
